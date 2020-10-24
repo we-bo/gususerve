f9f059b2111bc1b36fff4805d62a11b251b76f8f
we_bo <2636477115@qq.com>
2020-10-24 09:17:15 +0800

    后台1.0

--- app.js
+++ app.js
@@ -1,99 +1,111 @@
-const express=require('express');
-const app=express();
-const md5=require('md5');
-const cors=require('cors');
-const bodyParser=require('body-parser');
-const mysql=require('mysql');
-const pool=mysql.createPool({
+const express = require('express');
+const app = express();
+const md5 = require('md5');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const pool = mysql.createPool({
     //Mysql数据库服务器地址
-    host:'127.0.0.1',
+    host: '127.0.0.1',
     //端口号
-    port:'3306',
+    port: '3306',
     //数据库用户的用户名
-    user:'root',
+    user: 'root',
     //数据库用户的密码
-    password:'',
+    password: '',
     //数据库名称
-    database:'gspro',
+    database: 'gspro',
     //最大连接次数
-    connectionLimit:10
+    connectionLimit: 10
 })
 
 //cors跨域
 app.use(cors({
-    origin:['http://127.0.0.1:8080','http://127.0.0.1:8081','http://localhost:8080','http://localhost:8081']
+    origin: ['http://127.0.0.1:8080', 'http://127.0.0.1:8081', 'http://localhost:8080', 'http://localhost:8081']
 }));
 
 //应用中间件
 app.use(bodyParser.urlencoded({
-    extended:false
+    extended: false
 }));
 
 //轮播图分类的接口
-app.get('/category',(req,res)=>{
-    let sql='SELECT id,imgs FROM gspro_carousel';
-    pool.query(sql,(err,results)=>{
-        if(err) throw err;
-        res.send({message:'查询成功',code:1,results:results});
+app.get('/category', (req, res) => {
+    let sql = 'SELECT id,imgs FROM gspro_carousel';
+    pool.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send({ message: '查询成功', code: 1, results: results });
         //console.log(results);
     });
 });
 
 //房源分类接口
-app.get('/classify',(req,res)=>{
-    let sql='SELECT id,category FROM gspro_nav';
-    pool.query(sql,(err,results)=>{
-        if(err) throw err;
-        res.send({message:'查询成功',code:1,results:results});
+app.get('/classify', (req, res) => {
+    let sql = 'SELECT id,category FROM gspro_nav';
+    pool.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send({ message: '查询成功', code: 1, results: results });
         //console.log(results);
     });
 });
 
 //页面详情接口
-app.get('/list',(req,res)=>{
-    let id=req.query.id;
-    let sql ='SELECT id,images,title FROM gspro_holder WHERE nav_id=? ';
-    pool.query(sql,[id],(err,results)=>{
-        if(err) throw err;
-        res.send({message:'查询成功',code:1,results:results});
-        console.log(results);
+app.get('/list', (req, res) => {
+    let cid = req.query.cid;
+    let sql = 'SELECT id,images,title FROM gspro_holder WHERE nav_id=? ';
+    pool.query(sql, [cid], (err, results) => {
+        if (err) throw err;
+        res.send({ message: '查询成功', code: 1, results: results });
+        //console.log(results);
     })
 })
 
 //用户注册的接口
-app.post('/register',(req,res)=>{
-    let username=req.body.username;
-    let password=req.body.password;
-    let sql='SELECT id FROM gspro_user WHERE username=?'
-    pool.query(sql,[username],(err,results)=>{
-        if(err) throw err;
-        if(results.length == 0){
+app.post('/register', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    let sql = 'SELECT id FROM gspro_user WHERE username=?'
+    pool.query(sql, [username], (err, results) => {
+        if (err) throw err;
+        if (results.length == 0) {
             sql = 'INSERT INTO gspro_user(username,password) VALUES(?,MD5(?))';
-            pool.query(sql,[username,password],(err,results)=>{
-                if(err) throw err;
-                res.send({message:'注册成功',code:1});
+            pool.query(sql, [username, password], (err, results) => {
+                if (err) throw err;
+                res.send({ message: '注册成功', code: 1 });
             });
-        }else{
-            res.send({message:'注册失败',code:0});
+        } else {
+            res.send({ message: '注册失败', code: 0 });
         }
     });
 });
 
 //用户登录接口
-app.post('/login',(req,res)=>{
-    let username=req.body.username;
-    let password=md5(req.body.password);
-    let sql='SELECT id,username,nickname,avatar FROM gspro_user WHERE username=? AND password=?'
-    pool.query(sql,[username,password],(err,results)=>{
-        if(err) throw err;
-        if(results.length == 1){
-            res.send({message:'登陆成功',code:1,info:results[0]});
-        }else{
-            res.send({message:'登陆失败',code:0})
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = md5(req.body.password);
+    let sql = 'SELECT id,username,nickname,avatar FROM gspro_user WHERE username=? AND password=?'
+    pool.query(sql, [username, password], (err, results) => {
+        if (err) throw err;
+        if (results.length == 1) {
+            res.send({ message: '登陆成功', code: 1, info: results[0] });
+        } else {
+            res.send({ message: '登陆失败', code: 0 })
         }
+    });
+});
+
+//获取特定的房源信息的API接口
+app.get('/article', (req, res) => {
+    //获取房源id
+    let id = req.query.id;
+    //sql查询 -- 多表(内)连接
+    sql = 'SELECT u.id,images,avatar,nickname FROM gspro_user AS r INNER JOIN gspro_holder AS u on nav_id=r.id WHERE u.id=?';
+    pool.query(sql,[id],(err,results)=>{
+        if(err) throw err;
+        res.send({meaasge:'查询成功',code:1,results:results[0]});
+        //console.log(results)
     })
 })
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('server is runing...');
 })
--- gspro.sql
+++ gspro.sql
@@ -30,8 +30,10 @@ CREATE TABLE `gspro_carousel`(
 )ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `gspro_user`(`id`,`username`,`password`,`nickname`,`avatar`)VALUES(1,'是lisa啊','123123123','giao哥','/images/gs_icon2.jpg');
-INSERT INTO `gspro_user`(`id`,`username`,`password`,`nickname`,`avatar`)VALUES(2,'是jack啊','234234234','药水','/images/gs_icon2.jpg');
+INSERT INTO `gspro_user`(`id`,`username`,`password`,`nickname`,`avatar`)VALUES(1,'是lisa啊','123123123','tom','/images/gs_icon2.jpg');
+INSERT INTO `gspro_user`(`id`,`username`,`password`,`nickname`,`avatar`)VALUES(2,'是jack啊','234234234','小红','/images/gs_icon2.jpg');
+INSERT INTO `gspro_user`(`id`,`username`,`password`,`nickname`,`avatar`)VALUES(3,'是lisa啊','123123123','tom','/images/gs_icon2.jpg');
+INSERT INTO `gspro_user`(`id`,`username`,`password`,`nickname`,`avatar`)VALUES(4,'是jack啊','234234234','小红','/images/gs_icon2.jpg');
 
 INSERT INTO `gspro_nav`(`id`,`category`)VALUES(1,'精选');
 INSERT INTO `gspro_nav`(`id`,`category`)VALUES(2,'收藏热榜');
@@ -43,6 +45,25 @@ INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(2,'
 INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(3,'/images/201908311829492406_700_467.jpg','宋丹丹和铁锤的故事~',1,1);
 INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(4,'/images/201911072240514940_700_467.jpg','12天后,他将在这里举办婚礼',1,1);
 INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(5,'/images/202005060304171580_700_467.jpg','最美摩洛哥主题民宿，干净整洁',1,1);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(6,'/images/202005060304171580_700_467.jpg','最美摩洛哥主题民宿，干净整洁',2,2);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(7,'/images/202005060304171580_700_467.jpg','最美摩洛哥主题民宿，干净整洁',2,2);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(8,'/images/202005060304171580_700_467.jpg','最美摩洛哥主题民宿，干净整洁',2,2);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(9,'/images/202005060304171580_700_467.jpg','最美摩洛哥主题民宿，干净整洁',2,2);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(10,'/images/202005060304171580_700_467.jpg','最美摩洛哥主题民宿，干净整洁',2,2);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(11,'/images/201908311829492406_700_467.jpg','宋丹丹和铁锤的故事~',3,3);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(12,'/images/201908311829492406_700_467.jpg','宋丹丹和铁锤的故事~',3,3);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(13,'/images/201908311829492406_700_467.jpg','宋丹丹和铁锤的故事~',3,3);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(14,'/images/201908311829492406_700_467.jpg','宋丹丹和铁锤的故事~',3,3);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(15,'/images/201908311829492406_700_467.jpg','宋丹丹和铁锤的故事~',3,3);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(16,'/images/201903080011264795_700_467.jpg','北京|带话题的民宿，童话世界loft',4,4);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(17,'/images/201903080011264795_700_467.jpg','北京|带话题的民宿，童话世界loft',4,4);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(18,'/images/201903080011264795_700_467.jpg','北京|带话题的民宿，童话世界loft',4,4);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(19,'/images/201903080011264795_700_467.jpg','北京|带话题的民宿，童话世界loft',4,4);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(20,'/images/201903080011264795_700_467.jpg','北京|带话题的民宿，童话世界loft',4,4);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(21,'/images/201903080011264795_700_467.jpg','北京|带话题的民宿，童话世界loft',4,4);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(22,'/images/201903080011264795_700_467.jpg','北京|带话题的民宿，童话世界loft',3,3);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(23,'/images/201903080011264795_700_467.jpg','北京|带话题的民宿，童话世界loft',2,2);
+INSERT INTO `gspro_holder`(`id`,`images`,`title`,`nav_id`,`holder_id`)VALUES(24,'/images/201903080011264795_700_467.jpg','北京|带话题的民宿，童话世界loft',1,1);
 
 INSERT INTO `gspro_carousel`(`id`,`imgs`)VALUES(1,'/images/gs_lb1.jpg');
 INSERT INTO `gspro_carousel`(`id`,`imgs`)VALUES(2,'/images/gs_lb2.jpg');
